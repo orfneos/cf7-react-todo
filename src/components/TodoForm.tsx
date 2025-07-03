125056b6c824f706223867a5c79be4d9ae4ffd55
Nick Papadakis <orfneos@yahoo.gr>
2025-07-03 12:03:23 +0300

    feat: focus on input

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import type { TodoFormProps } from "../types";
 
 const TodoForm = ({dispatch, inputRef}: TodoFormProps) => {
